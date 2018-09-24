99eed56570bf45e77838b56a848242a6e20cc7e8
Mingala <lcambou@wanadoo.fr>
2018-09-25 00:02:43 +0100

    Update docker_entrypoint.sh

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/ash
+#!/bin/bash
 # setup MariaDB if no mysql database found
 if [ ! -d "$MYSQL_DATABASE/mysql" ]; then
-	#10.3 parameters change
-	/usr/bin/mysql_install_db --defaults-extra-file==$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE --skip-test-db
+	# 10.3 parameters change
+	/usr/bin/mysql_install_db --defaults-extra-file=$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE --skip-test-db
 fi
 # run mysql database
 /usr/bin/mysqld_safe --defaults-extra-file=$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE
